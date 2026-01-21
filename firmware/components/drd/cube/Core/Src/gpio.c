852b8950d303f421df2e70d6f899e76fec017980
Tony Chen <81488561+tonychen-2006@users.noreply.github.com>
2026-01-20 20:29:57 -0800

    Feat: Added DRD components for the new v4 schematic. (#8)
    * Added DRD components as per the new v4 schematic.
    
    * Added to gitignore.
    
    * Delete firmware/components/drd/.DS_Store

--- firmware/components/drd/cube/Core/Src/gpio.c
+++ firmware/components/drd/cube/Core/Src/gpio.c
@@ -42,10 +42,61 @@
 void MX_GPIO_Init(void)
 {
 
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOD_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, DEBUG_LED_Pin|DEBUG_LEDA1_Pin|DISPLAY_A0_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, DISPLAY_RESET_Pin|RTS_OUT_Pin|ESTOP_Pin|BL_LIGHTS_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOC, BR_LIGHTS_Pin|FLT_MCU_Pin|LTS_OUT_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : DEBUG_LED_Pin DEBUG_LEDA1_Pin DISPLAY_A0_Pin */
+  GPIO_InitStruct.Pin = DEBUG_LED_Pin|DEBUG_LEDA1_Pin|DISPLAY_A0_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : DISPLAY_RESET_Pin RTS_OUT_Pin ESTOP_Pin BL_LIGHTS_Pin */
+  GPIO_InitStruct.Pin = DISPLAY_RESET_Pin|RTS_OUT_Pin|ESTOP_Pin|BL_LIGHTS_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : BR_LIGHTS_Pin FLT_MCU_Pin LTS_OUT_Pin */
+  GPIO_InitStruct.Pin = BR_LIGHTS_Pin|FLT_MCU_Pin|LTS_OUT_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : BRK_IN_Pin */
+  GPIO_InitStruct.Pin = BRK_IN_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(BRK_IN_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : DRIVE_STATE_NEXT_Pin DRIVE_STATE_PREV_Pin */
+  GPIO_InitStruct.Pin = DRIVE_STATE_NEXT_Pin|DRIVE_STATE_PREV_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : HAZARD_Pin ECO_POWER_Pin */
+  GPIO_InitStruct.Pin = HAZARD_Pin|ECO_POWER_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
 }
 

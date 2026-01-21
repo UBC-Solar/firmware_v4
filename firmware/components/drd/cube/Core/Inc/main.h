168774fc3533902375b38b9f40f3378993756cae
Tony Chen <tonychengangyi@gmail.com>
2026-01-20 20:02:53 -0800

    Added DRD components as per the new v4 schematic.

--- firmware/components/drd/cube/Core/Inc/main.h
+++ firmware/components/drd/cube/Core/Inc/main.h
@@ -57,6 +57,40 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define DEBUG_LED_Pin GPIO_PIN_0
+#define DEBUG_LED_GPIO_Port GPIOA
+#define DEBUG_LEDA1_Pin GPIO_PIN_1
+#define DEBUG_LEDA1_GPIO_Port GPIOA
+#define DISPLAY__CS_Pin GPIO_PIN_4
+#define DISPLAY__CS_GPIO_Port GPIOA
+#define DISPLAY_SCL_Pin GPIO_PIN_5
+#define DISPLAY_SCL_GPIO_Port GPIOA
+#define DISPLAY_A0_Pin GPIO_PIN_6
+#define DISPLAY_A0_GPIO_Port GPIOA
+#define DISPLAY_RESET_Pin GPIO_PIN_10
+#define DISPLAY_RESET_GPIO_Port GPIOB
+#define RTS_OUT_Pin GPIO_PIN_12
+#define RTS_OUT_GPIO_Port GPIOB
+#define ESTOP_Pin GPIO_PIN_14
+#define ESTOP_GPIO_Port GPIOB
+#define BL_LIGHTS_Pin GPIO_PIN_15
+#define BL_LIGHTS_GPIO_Port GPIOB
+#define BR_LIGHTS_Pin GPIO_PIN_6
+#define BR_LIGHTS_GPIO_Port GPIOC
+#define FLT_MCU_Pin GPIO_PIN_7
+#define FLT_MCU_GPIO_Port GPIOC
+#define LTS_OUT_Pin GPIO_PIN_8
+#define LTS_OUT_GPIO_Port GPIOC
+#define BRK_IN_Pin GPIO_PIN_9
+#define BRK_IN_GPIO_Port GPIOC
+#define DRIVE_STATE_NEXT_Pin GPIO_PIN_10
+#define DRIVE_STATE_NEXT_GPIO_Port GPIOA
+#define DRIVE_STATE_PREV_Pin GPIO_PIN_11
+#define DRIVE_STATE_PREV_GPIO_Port GPIOA
+#define HAZARD_Pin GPIO_PIN_6
+#define HAZARD_GPIO_Port GPIOB
+#define ECO_POWER_Pin GPIO_PIN_7
+#define ECO_POWER_GPIO_Port GPIOB
 
 /* USER CODE BEGIN Private defines */
 

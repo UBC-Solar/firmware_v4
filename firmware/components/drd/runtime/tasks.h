163d386b2f1e88aed89453170788d9b5b5964492
Tony Chen <81488561+tonychen-2006@users.noreply.github.com>
2026-02-05 20:38:17 -0800

    Feat: Added drive state freertos task. (#13)
    * Added drive state freertos task.
    
    * Added the path for the drive state.
    
    * Included path in cmake file.
    
    * Changed folder directory
    
    * Added custom drive state task.
    
    * Fixed naming convention.

--- firmware/components/drd/runtime/tasks.h
+++ firmware/components/drd/runtime/tasks.h
@@ -1,4 +1,14 @@
 #ifndef __TASKS_H__
 #define __TASKS_H__
 
+/* TASK FUNCTION PROTOTYPES */
+
+/**
+ * @brief Drive state task main loop.
+ * 
+ * Manages the drive system state machine and control logic.
+ * Executes with at a 1ms period. Task runs indefinitely until system shutdown.
+ */
+void TasksDriveState(void);
+
 #endif //__TASKS_H__

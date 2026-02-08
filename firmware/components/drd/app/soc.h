5e36669366a5d64de40517ee2c15e934f4507ebe
Tony Chen <tonychengangyi@gmail.com>
2026-02-07 16:38:14 -0800

    Added soc files.

---
+++ firmware/components/drd/app/soc.h
@@ -0,0 +1,23 @@
+#ifndef __SOC_H__
+#define __SOC_H__
+
+/* INCLUDES */
+#include <stdint.h>
+
+/* DEFINES */
+#define TIME_STEP           (100.0f)
+#define SOC_TIME_STEP       (TIME_STEP / 1000.0f)   // ms
+
+/* GLOBALS */
+extern volatile float g_total_pack_voltage_soc;
+extern volatile float g_pack_current_soc;
+
+/* FUNCTION DECLARATIONS */
+void SOC_predict_then_update(float g_total_pack_voltage_soc, float g_pack_current_soc, float time_step);
+float SOC_get_soc();        // 0 - 1
+float SOC_get_voltage();
+float SOC_get_Uc();
+void SOC_init_soc(int voltage);
+
+
+#endif //__SOC_H__

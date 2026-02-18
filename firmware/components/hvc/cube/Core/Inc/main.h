6b2c384f523bf3c6191cefe6b8df33bd568b7e4a
Christopher Kalitin <Christopher.Kalitin@gmail.com>
2026-02-17 19:33:18 -0800

    Configure ioc pins

--- firmware/components/hvc/cube/Core/Inc/main.h
+++ firmware/components/hvc/cube/Core/Inc/main.h
@@ -49,6 +49,8 @@ extern "C" {
 
 /* USER CODE END EM */
 
+void HAL_TIM_MspPostInit(TIM_HandleTypeDef *htim);
+
 /* Exported functions prototypes ---------------------------------------------*/
 void Error_Handler(void);
 
@@ -57,6 +59,66 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define GPIO1_Pin GPIO_PIN_0
+#define GPIO1_GPIO_Port GPIOC
+#define GPIO2_Pin GPIO_PIN_1
+#define GPIO2_GPIO_Port GPIOC
+#define IMD_GPIO_IN_Pin GPIO_PIN_2
+#define IMD_GPIO_IN_GPIO_Port GPIOC
+#define THERMISTOR_Pin GPIO_PIN_1
+#define THERMISTOR_GPIO_Port GPIOA
+#define MOTOR_PRECHARGE_Pin GPIO_PIN_4
+#define MOTOR_PRECHARGE_GPIO_Port GPIOA
+#define MPPT_PRECHARGE_Pin GPIO_PIN_5
+#define MPPT_PRECHARGE_GPIO_Port GPIOA
+#define MASTERBOARD_FAULT_Pin GPIO_PIN_6
+#define MASTERBOARD_FAULT_GPIO_Port GPIOA
+#define SUPP_SENSE_Pin GPIO_PIN_7
+#define SUPP_SENSE_GPIO_Port GPIOA
+#define ESTOP_Pin GPIO_PIN_4
+#define ESTOP_GPIO_Port GPIOC
+#define DCDC_ACTIVE_Pin GPIO_PIN_5
+#define DCDC_ACTIVE_GPIO_Port GPIOC
+#define SUPP_ACTIVE_Pin GPIO_PIN_0
+#define SUPP_ACTIVE_GPIO_Port GPIOB
+#define LV_CURR_SENSE_Pin GPIO_PIN_1
+#define LV_CURR_SENSE_GPIO_Port GPIOB
+#define BOOT1_Pin GPIO_PIN_2
+#define BOOT1_GPIO_Port GPIOB
+#define POS_CTRL_Pin GPIO_PIN_10
+#define POS_CTRL_GPIO_Port GPIOB
+#define LLIM_CTRL_Pin GPIO_PIN_11
+#define LLIM_CTRL_GPIO_Port GPIOB
+#define MOTOR_PC_CTRL_Pin GPIO_PIN_12
+#define MOTOR_PC_CTRL_GPIO_Port GPIOB
+#define HLIM_CTRL_Pin GPIO_PIN_13
+#define HLIM_CTRL_GPIO_Port GPIOB
+#define MPPT_PC_CTRL_Pin GPIO_PIN_14
+#define MPPT_PC_CTRL_GPIO_Port GPIOB
+#define DISCHARGE_TOGGLE_OFF_Pin GPIO_PIN_15
+#define DISCHARGE_TOGGLE_OFF_GPIO_Port GPIOB
+#define NEG_CTRL_Pin GPIO_PIN_6
+#define NEG_CTRL_GPIO_Port GPIOC
+#define ESTOP_LED_Pin GPIO_PIN_7
+#define ESTOP_LED_GPIO_Port GPIOC
+#define FAULT_LED_Pin GPIO_PIN_8
+#define FAULT_LED_GPIO_Port GPIOC
+#define SUPP_LOW_LED_Pin GPIO_PIN_9
+#define SUPP_LOW_LED_GPIO_Port GPIOC
+#define DEBUG_LED_Pin GPIO_PIN_8
+#define DEBUG_LED_GPIO_Port GPIOA
+#define MPPT_CTRL_Pin GPIO_PIN_9
+#define MPPT_CTRL_GPIO_Port GPIOA
+#define DIST_CTRL_Pin GPIO_PIN_10
+#define DIST_CTRL_GPIO_Port GPIOA
+#define HV_CURRENT_ALERT_Pin GPIO_PIN_10
+#define HV_CURRENT_ALERT_GPIO_Port GPIOC
+#define IMD_CTRL_Pin GPIO_PIN_11
+#define IMD_CTRL_GPIO_Port GPIOC
+#define FAN_PWM_Pin GPIO_PIN_8
+#define FAN_PWM_GPIO_Port GPIOB
+#define FAN_CTRL_Pin GPIO_PIN_9
+#define FAN_CTRL_GPIO_Port GPIOB
 
 /* USER CODE BEGIN Private defines */
 

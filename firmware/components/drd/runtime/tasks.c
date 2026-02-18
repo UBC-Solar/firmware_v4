7ff30453701ca3e6c8c33981ff5eff8c58a2ec5c
Gregory Bian <gregorybian100@gmail.com>
2026-02-17 20:49:42 -0800

    first commit

--- firmware/components/drd/runtime/tasks.c
+++ firmware/components/drd/runtime/tasks.c
@@ -1,8 +1,10 @@
 #include "tasks.h"
+#include "cmsis_os2.h"
 
 /* DRIVE STATE TASK */
-void TasksDriveState(void) {
-    for(;;)
+void TasksDriveState(void)
+{
+    for (;;)
     {
         // function calls begin here
         osDelay(1);

f882e047d00b73156549a95d85d4f1281fb8c75c
Tony Chen <tonychengangyi@gmail.com>
2026-02-19 18:28:46 -0800

    Added CAN functionality back.

--- firmware/components/drd/driver/can_driver.c
+++ firmware/components/drd/driver/can_driver.c
@@ -2,6 +2,9 @@
 #include "CAN_comms.h"
 #include "can.h"
 
+/* FUNCTION PROTOTYPES */
+void VechicleStateCANRxHandler(uint32_t msg_id, uint8_t* data);
+
 /**
  *  CAN Message Header for drive control
  */
@@ -16,3 +19,69 @@ const CAN_TxHeaderTypeDef mdu_request_header = {.StdId = 0,
                                                 .IDE = CAN_ID_EXT,
                                                 .RTR = CAN_RTR_DATA,
                                                 .DLC = MDU_REQUEST_SIZE};
+
+/**
+ * @brief Initializes the CAN filter and CAN Rx callback function as CAN_comms_Rx_callback().
+ *
+ * Note: This uses the CAN_comms abstraction layer which will initialize two freeRTOS tasks. As a result it is recommended to
+ * Call this function inside the MX_FREERTOS_Init() function in freertos.c
+ */
+void CAN_tasks_init()
+{
+    CAN_comms_config_t CAN_comms_config_tel = {0};
+    // CAN_FilterTypeDef can_filter = {0};
+    // CAN_filter_init(&can_filter);
+
+    CAN_comms_config_tel.hcan = &hcan;
+    // CAN_comms_config_tel.CAN_Filter = can_filter;
+    CAN_comms_config_tel.CAN_comms_Rx_callback = CAN_comms_Rx_callback;
+
+    CAN_comms_init(&CAN_comms_config_tel);
+}
+
+
+void CAN_comms_Rx_callback(CAN_comms_Rx_msg_t* CAN_comms_Rx_msg)
+{
+	uint32_t CAN_ID = 0;
+	/*
+	 *	handle parsing rx messages
+	 */
+	if (CAN_comms_Rx_msg == NULL)
+	{
+			return;
+	}
+
+	if(CAN_comms_Rx_msg->header.IDE == CAN_ID_EXT)
+	{
+		CAN_ID = CAN_comms_Rx_msg->header.ExtId; // Get CAN ID
+	}
+	else
+	{
+		CAN_ID = CAN_comms_Rx_msg->header.StdId; // Get CAN ID
+	}
+
+	VechicleStateCANRxHandler(CAN_ID, CAN_comms_Rx_msg->data);
+}
+
+/* CAN RX */
+void VechicleStateCANRxHandler(uint32_t msg_id, uint8_t* data)
+{
+
+    switch (msg_id)
+    {
+    case FRAME0:
+        VelocityHandler(data);
+        break;
+    case STR_CAN_MSG_ID:
+        SteeringCanMsgHandler(data);
+        break;
+
+#ifdef DEBUG
+    case 0x500:
+        StateRequestCanMsgHandler(data);
+        break;
+#endif
+    }
+}
+
+/* CAN TX */

b9ae41fb90078c26791b31b29f426a59cda01dc2
Christopher Kalitin <Christopher.Kalitin@gmail.com>
2026-03-01 18:27:59 -0800

    DMA UART

--- firmware/components/hvc/app/hvc_main.c
+++ firmware/components/hvc/app/hvc_main.c
@@ -4,10 +4,5 @@
 
 void hvcMain(void)
 {
-    uint32_t sum = 1;
-    DEBUG_IO_PRINT("test,%d\n\r", sum);
-    DEBUG_IO_PRINT("Hello from hvcMain0!\r\n");
-    DEBUG_IO_PRINT("Hello from hvcMain2!\n\r");
-    uint32_t time = HAL_GetTick();
-    DEBUG_IO_PRINT("Current time: %d ms\n\r", time);
+    
 }

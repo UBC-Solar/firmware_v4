8df730e2206dab15781203616d25f00917859db5
Christopher Kalitin <Christopher.Kalitin@gmail.com>
2026-03-04 12:19:40 -0800

    Delete whitespace

--- firmware/components/hvc/app/hvc_main.c
+++ firmware/components/hvc/app/hvc_main.c
@@ -1,9 +1,9 @@
-#include "debug_io.h"
-#include "uart_driver.h"
-#include "adc_driver.h"
 #include "stm32f1xx_hal.h"
 #include <stdint.h>
 
+#include "debug_io.h"
+#include "uart_driver.h"
+#include "adc_driver.h"
 
 void HVC_Init(UART_HandleTypeDef *_huart2, ADC_HandleTypeDef *_hadc1, TIM_HandleTypeDef *_htim3) {
     UART_Init(_huart2);

da7cf04cc28360514828907a8933c915ec174a02
Christopher De Lazzari <c.de.lazzari03@gmail.com>
2026-03-28 14:31:43 -0700

    HVC GPIO Toggle function (#35)

--- firmware/components/hvc/driver/gpio_driver.c
+++ firmware/components/hvc/driver/gpio_driver.c
@@ -1,11 +1,12 @@
 #include "gpio_driver.h"
+#include "stm32f1xx_hal_gpio.h"
 #include "uart_driver.h"
 #include "main.h"
 
 /**
  * @brief Read the current logic state of a GPIO pin.
- * @param port GPIO port instance (for example, GPIOA, GPIOB).
- * @param pin GPIO pin mask (for example, GPIO_PIN_5).
+ * @param port GPIO port instance (for example, DIST_CTRL_GPIO_Port).
+ * @param pin GPIO pin mask (for example, DIST_CTRL_Pin).
  * @return GPIO pin state as GPIO_PIN_SET or GPIO_PIN_RESET.
  */
 GPIO_PinState GPIO_Read(GPIO_TypeDef *port, uint16_t pin) {
@@ -14,8 +15,8 @@ GPIO_PinState GPIO_Read(GPIO_TypeDef *port, uint16_t pin) {
 
 /**
  * @brief Write a logic state to a GPIO pin.
- * @param port GPIO port instance (for example, GPIOA, GPIOB).
- * @param pin GPIO pin mask (for example, GPIO_PIN_5).
+ * @param port GPIO port instance (for example, DIST_CTRL_GPIO_Port).
+ * @param pin GPIO pin mask (for example, DIST_CTRL_Pin).
  * @param state Output state to write: GPIO_PIN_SET or GPIO_PIN_RESET.
  */
 void GPIO_Write(GPIO_TypeDef *port, uint16_t pin, GPIO_PinState state) {
@@ -23,8 +24,18 @@ void GPIO_Write(GPIO_TypeDef *port, uint16_t pin, GPIO_PinState state) {
 }
 
 /**
+ * @brief Toggle the logic state of a GPIO pin.
+ * @param port GPIO port instance (for example, DIST_CTRL_GPIO_Port).
+ * @param pin GPIO pin mask (for example, DIST_CTRL_Pin).
+ */
+void GPIO_TogglePin(GPIO_TypeDef *port, uint16_t pin) {
+    HAL_GPIO_TogglePin(port, pin);
+}
+
+
+/**
  * @brief HAL callback invoked on external interrupt line events.
- * @param GPIO_Pin The pin number that triggered the interrupt (for example, GPIO_PIN_5).
+ * @param GPIO_Pin The pin number that triggered the interrupt (for example, DIST_CTRL_Pin).
  */
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
     GPIO_PinState level;
--- firmware/components/hvc/driver/gpio_driver.h
+++ firmware/components/hvc/driver/gpio_driver.h
@@ -4,3 +4,4 @@
 
 GPIO_PinState GPIO_Read(GPIO_TypeDef *port, uint16_t pin);
 void GPIO_Write(GPIO_TypeDef *port, uint16_t pin, GPIO_PinState state);
+void GPIO_TogglePin(GPIO_TypeDef *port, uint16_t pin);

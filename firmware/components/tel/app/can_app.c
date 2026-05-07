a774fe7154f2555e0ed3fb27e4cee0058a5814d7
Gregory Bian <gregorybian100@gmail.com>
2026-05-06 19:46:25 -0700

    small syntax fix

--- firmware/components/tel/app/can_app.c
+++ firmware/components/tel/app/can_app.c
@@ -9,6 +9,14 @@
  */
 static void CanAppRxCallback(CAN_comms_Rx_msg_t* CAN_comms_Rx_msg);
 
+/**
+ * @brief RTC Driver CAN RX Handler for processing RTC-related CAN messages.
+ * @param msg_id The CAN message ID.
+ * @param data Pointer to the received CAN message data.
+ */
+static void RtcDriverRxHandler(uint32_t msg_id, uint8_t* data);
+
+
 void CanAppInit(){
     // Initialize CAN Comms RX Callback function 
     CAN_comms_config_t CAN_comms_config_tel = CanDriverInit();
@@ -34,10 +42,10 @@ static void CanAppRxCallback(CAN_comms_Rx_msg_t* CAN_comms_Rx_msg){
 		CAN_ID = CAN_comms_Rx_msg->header.StdId; // Get standard CAN ID
 	}
     // RX Functions go here
-	RtcDriverRxHandler(CAN_ID, CAN_comms_Rx_msg->data)
+	RtcDriverRxHandler(CAN_ID, CAN_comms_Rx_msg->data);
 }
 
-void RtcDriverRxHandler(uint32_t msg_id, uint8_t* data)
+static void RtcDriverRxHandler(uint32_t msg_id, uint8_t* data)
 {
     switch (msg_id)
     {

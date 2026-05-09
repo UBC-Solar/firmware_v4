fa0d5daf1072d6f7ed57e772afd29aef48c91d73
Gregory Bian <gregorybian100@gmail.com>
2026-05-09 15:16:51 -0700

    fixed comments

--- firmware/components/tel/app/can_app.c
+++ firmware/components/tel/app/can_app.c
@@ -9,13 +9,6 @@
  */
 static void CanAppRxCallback(CAN_comms_Rx_msg_t* CAN_comms_Rx_msg);
 
-/**
- * @brief RTC Driver CAN RX Handler for processing RTC-related CAN messages.
- * @param msg_id The CAN message ID.
- * @param data Pointer to the received CAN message data.
- */
-static void RtcDriverRxHandler(uint32_t msg_id, uint8_t* data);
-
 
 void CanAppInit(){
     // Initialize CAN Comms RX Callback function 
@@ -42,17 +35,6 @@ static void CanAppRxCallback(CAN_comms_Rx_msg_t* CAN_comms_Rx_msg){
 		CAN_ID = CAN_comms_Rx_msg->header.StdId; // Get standard CAN ID
 	}
     // RX Functions go here
-	RtcDriverRxHandler(CAN_ID, CAN_comms_Rx_msg->data);
+	RtcAppRxHandler(CAN_ID, CAN_comms_Rx_msg->data);
 }
 
-static void RtcDriverRxHandler(uint32_t msg_id, uint8_t* data)
-{
-    switch (msg_id)
-    {
-    case RTC_TIMESTAMP_MSG_ID:
-        RtcAppSyncMemoratorToTel(data);
-        break;
-    default:
-        break;
-    }
-}
--- firmware/components/tel/app/rtc_app.c
+++ firmware/components/tel/app/rtc_app.c
@@ -1,6 +1,6 @@
 #include "rtc_app.h"
+#include "can_app.h"
 #include "rtc_driver.h"
-#include <sys/types.h>
 
 static bool rtc_initialized = false;
 
@@ -9,8 +9,29 @@ void RtcAppSyncMemoratorToTel(uint8_t *data) {
         // If RTC is already initialized, we can ignore further sync messages
         return;
     }
-    rtc_initialized = true; // Set the RTC initialized flag to true when we receive data to sync
+    RtcDriverTimeData rtc_time_data = {0};
+    RtcDriverDateData rtc_date_data = {0};
 
-    RtcDriverSetTime(data);
-    RtcDriverSetDate(data);
+    rtc_time_data.seconds = data[TIMETYPEDEF_SECONDS_IDX];
+    rtc_time_data.minutes = data[TIMETYPEDEF_MINUTES_IDX];
+    rtc_time_data.hours = data[TIMETYPEDEF_HOURS_IDX];
+    rtc_date_data.day = data[TIMETYPEDEF_DAY_IDX];
+    rtc_date_data.month = data[TIMETYPEDEF_MONTH_IDX];
+    rtc_date_data.year = data[TIMETYPEDEF_YEAR_IDX];
+
+    RtcDriverSetTime(&rtc_time_data);
+    RtcDriverSetDate(&rtc_date_data);
+    rtc_initialized = true; // Set the RTC initialized flag to true when we receive data to sync
+}
+
+void RtcAppRxHandler(uint32_t msg_id, uint8_t* data)
+{
+    switch (msg_id)
+    {
+    case RTC_TIMESTAMP_MSG_ID:
+        RtcAppSyncMemoratorToTel(data);
+        break;
+    default:
+        break;
+    }
 }

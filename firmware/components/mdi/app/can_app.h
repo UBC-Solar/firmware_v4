f43f87474bef9e8e240c6d4cb8bd66d45e1d9e5b
Martin Wu <martinwu500@gmail.com>
2026-05-16 15:11:00 -0700

    Fixed build

--- firmware/components/mdi/app/can_app.h
+++ firmware/components/mdi/app/can_app.h
@@ -8,8 +8,6 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-#include "mdi_driver.h"
-
 void CanAppInit(void);
 bool CanAppTryGetMotorCommand(MdiMotorCommand *command);
 uint32_t CanAppGetLastCommandTick(void);
---
+++ firmware/components/mdi/driver/mdi_driver.h
@@ -0,0 +1,42 @@
+/**
+ * @file    mdi_driver.h
+ * @brief   MDI hardware control definitions.
+ */
+#ifndef __MDI_DRIVER_H__
+#define __MDI_DRIVER_H__
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#define MDI_MAX_DAC_VALUE ((uint16_t)(0.90f * 0x3FFU))
+#define MDI_MAX_TIMEOUT_VALUE 100U
+#define MDI_DIAGNOSTICS_DELAY 1000U
+
+typedef enum {
+    MDI_DAC_REGEN = (0b0001101u << 1),
+    MDI_DAC_ACCEL = (0b0001110u << 1)
+} MdiDacAddr;
+
+typedef enum {
+    MDI_DIRECTION_FORWARD = 0,
+    MDI_DIRECTION_REVERSE = 1
+} MdiDirection;
+
+typedef enum {
+    MDI_POWER_MODE = 0,
+    MDI_ECO_MODE = 1
+} MdiPowerMode;
+
+typedef struct {
+    uint16_t accel_DAC_value;
+    uint16_t regen_DAC_value;
+    bool direction_value;
+    bool eco_mode_value;
+} MdiMotorCommand;
+
+void MdiSetDacVoltage(MdiDacAddr dac_addr, uint16_t voltage_value);
+void MdiSetMotorCommand(const MdiMotorCommand *command);
+void MdiParseMotorCommand(const uint8_t *buffer, MdiMotorCommand *command);
+void MdiStopMotor(void);
+
+#endif /* __MDI_DRIVER_H__ */

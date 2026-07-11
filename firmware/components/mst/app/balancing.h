ce28a894cb8395d5a962d6a132c11523bc360d89
Michael Lin <michaellin0902@gmail.com>
2026-07-11 12:58:58 -0700

    Fix: Force-disable balancing pins on ADBMS when balancing is not enabled on MST

--- firmware/components/mst/app/balancing.h
+++ firmware/components/mst/app/balancing.h
@@ -7,6 +7,6 @@
 void DoBalancing(pack_state_t *pack_state, module_t pack_modules[NUM_MODULES], slave_t slaves[SLAVE_NUM_DEVICES]);
 void PauseAllBalancing();
 void ResumeAllBalancing();
-#if (INT_TEST_SLAVE == RUN)
+#if (INT_TEST_SLAVE == RUN || INT_TEST_SLAVE_BAL_VOLT == RUN)
 void Debug_DoBalancing(slave_t slaves[SLAVE_NUM_DEVICES], bool enable);
-#endif // UNIT_TEST_ISOSPI
+#endif // (INT_TEST_SLAVE == RUN || INT_TEST_SLAVE_BAL_VOLT == RUN)

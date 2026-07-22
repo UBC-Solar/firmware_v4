d2b23c7f138c674344357286827800b0723d0cd8
Michael Lin <michaellin0902@gmail.com>
2026-07-21 20:37:26 -0700

    Fix: MST shouldn't report data until it measures everything

--- firmware/components/mst/app/mst_defs.h
+++ firmware/components/mst/app/mst_defs.h
@@ -64,6 +64,10 @@
 // Runs the mainloop. Should be enabled unless running a test
 #define RUN_MAIN_LOOP true
 
+// Number of times mainloop has to run before we have all data.
+// This is useful if we don't plan on measuring all cell data on every loop iteration
+#define NUM_INIT_MAINLOOPS 4
+
 // If false, will not halt even if IsoSPI comms fail
 #define ISOSPI_CONNECTED true
 
@@ -80,7 +84,7 @@
 
 // During testing, slaveboard / SBT temperature circuitry were damaged
 // and report incorrect temperatures. If true, ignore all temperature warnings / faults
-#define IGNORE_TEMPEREATURE_VALUES true
+#define IGNORE_TEMPEREATURE_VALUES false
 
 
 /**
